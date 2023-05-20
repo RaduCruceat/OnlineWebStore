055c2ce63190b85a68c9f2c1ceeb0b6c64dec509
RaduCruceatULBS <127135458+RaduCruceatULBS@users.noreply.github.com>
2023-05-20 16:15:40 +0300

    removed private config

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBFjSyqoXYs3fqbv8mAe1jS9vsVo8V988U",
-  authDomain: "magazindelego.firebaseapp.com",
-  projectId: "magazindelego",
-  storageBucket: "magazindelego.appspot.com",
-  messagingSenderId: "677310682773",
-  appId: "1:677310682773:web:5d6ebe90e9d56e8bf52d9c"
-};
